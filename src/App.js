9da51b3628a187fb0fedf2e615bb939335f6701c
Anushree Das <64682931+meAnushree@users.noreply.github.com>
2022-03-24 20:08:08 +0530

    add monster-rolodex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react/cjs/react.development";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monster: [
+        {
+          name: "Monter1",
+          id: "mon1",
+        },
+        {
+          name: "Monter2",
+          id: "mon1",
+        },
+        {
+          name: "Monter3",
+          id: "mon1",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.monster.map((monster) => (
+          <h1 key={monster.id}> {monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
-
 export default App;
